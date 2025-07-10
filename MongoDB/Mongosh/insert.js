c0de34718fcaa55414e4e272fc2fd718c022eb02
Simran <ss1043422@gmail.com>
2025-07-10 10:23:55 +0530

    Insert Using Shell

---
+++ MongoDB/Mongosh/insert.js
@@ -0,0 +1,19 @@
+// for running mongodb shell first 
+// mongod--dbpath=data then mongosh(shell) just like cmd line 
+// for creating db - use db_name
+// for collection inside db db.ccreateCollection("name_col");
+// insert - db.collection.insertOne({object full})
+
+// const teacher = [
+//     {name:'kartik',tech:'web dev'},
+//     {name:'monu',tech:'java'},
+//     {name:'abhishek',tech:'c++'},
+//     {name:'mayank',tech:'cp'}
+// ]
+// [{name:'abhishek',tech:'c++'},
+//     {name:'mayank',tech:'cp'}]
+
+let products =[
+    {name:'laptop',feature:['touchPad','i-6'],price:40},
+    {name:'laptop',price:100}
+] 
